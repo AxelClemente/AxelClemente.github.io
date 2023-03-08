7fb51c9755009d877b24c9ea8bba4d2cfc08c254
Axel <axel_clemente@yahoo.com>
2023-03-08 13:40:53 +0100

    Sprit img working

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const numberOfParticles = 200;
+const numberOfParticles = 50;
 let particlesArray = [];
 
 //DIBUJAMOS NUESTRO OBJECTO USANDO JAVASCRIPT
 const pumpkin = new Image();
 pumpkin.src = 'Images/Images/monster.png'
+
+//Sprit img
+const pumpkins = new Image();
+pumpkins.src = 'Images/Images/pumpkings.png'
 
 
 
@@ -16,10 +20,14 @@ class Particle{
     constructor(){
         this.x = Math.random() * canvas.width; //LAS PARTICULAS APARECERAN DE FORMA ALEATORIA EN TODO EL EJE X DEL CANVAS
         this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 20 + 50;
+        this.size = Math.random() * 150 + 50;
         this.speed = Math.random() * 3 + 1;
         this.angle = Math.random() * 360;
         this.spin = Math.random() < 0.5 ? -1 : 1; // LAS PARTICULAS GIRAN A LA IZQUIERDA SI EL NUMERO RANDOM ES NEGATIVO Y VICEVERSA
+         // SPRITE IMG
+        this.frameX = Math.floor(Math.random() * 3);
+        this.frameY = Math.floor(Math.random() * 3);
+        this.spriteSize = 900/3;
     }
 
     draw(){
@@ -29,17 +37,19 @@ class Particle{
         ctx.rotate(this.angle * Math.PI/360 * this.spin);
         // ctx.fillStyle = 'red'
         // ctx.fillRect(0,0, canvas.width, canvas.height); 
-        ctx.drawImage(pumpkin, 0 - this.size/2, 0 - this.size/2, this.size, this.size)
+        // ctx.drawImage(pumpkin, 0 - this.size/2, 0 - this.size/2, this.size, this.size)
+        ctx.drawImage(pumpkins, this.frameX * this.spriteSize, this.frameY * this.spriteSize, this.spriteSize, this.spriteSize, 0 - this.size/2, 0 - this.size/2, this.size, this.size);
         ctx.restore(); // *** PARA PODER TRABAJAR CON EL TRANSLATE *** // IMPORTANTE
     }
 
     update(){
         this.angle += 2;
-        if (this.y > canvas.height){
+        if (this.y - this.size > canvas.height){
             this.y = 0 - this.size; //<---------------CHECK NO SE QUE HACE REALMENTE
             this.x = Math.random() * canvas.width;
             this.size = Math.random() * 20 + 50;
             this.speed = Math.random() * 3 + 1;
+           
         }
         this.y += this.speed; // UPDATE CREAMOS EL EFECTO DE CAIDA
     }
