67a3aced9bea3563521fa8bbc5db9682a72e256c
Axel <axel_clemente@yahoo.com>
2023-03-11 13:35:03 +0100

    landing page in progress

--- script.js
+++ script.js
@@ -532,9 +532,43 @@ init();
 // }
 // animationId = requestAnimationFrame(animate);
 
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*======== CANVAS 2D GAME ========*/
+
 document.querySelector("button").addEventListener("click", () => {
     document.querySelector("button").style.display = "none";
     canvas.classList.remove("canvas-hidden");
+    document.querySelector(".canvas-hidden").classList.remove("canvas-hidden");
     let animationId;
     function animate(){    
         ctx.clearRect(0, 0, canvas.width, canvas.height);
